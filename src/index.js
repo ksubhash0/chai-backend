8e4af8564f3ef52161827b3ed390909bfc11d8e0
ksubhash0 <ksubhash079@gmail.com>
2025-07-02 22:30:37 +0530

    utils class creted

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import 'dotenv/config';
+import { app } from './app.js';
 
 
 import connectDb from "./db/index.js";
 
-connectDb();
+connectDb().then(() => {
+    app.listen(process.env.PORT || 8080, () => {
+        console.log('App Running...')
+    })
+}).catch((err) => {
+    console.log('Database connection failed: ', err)
+})
